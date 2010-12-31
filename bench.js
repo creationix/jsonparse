1352a99122cb5c8d8cf1bc12f64c8643ea825963
Tim Caswell <tim@creationix.com>
2010-12-31 11:50:24 -0800

    Run the bench in a loop for better results
    Change-Id: Ie9d99631f0dd507e4647de1b441d6886f51b6bba

--- bench.js
+++ bench.js
@@ -3,15 +3,21 @@ var fs = require('fs'),
 
 
 var json = fs.readFileSync("samplejson/basic.json");
-var start = Date.now();
-for (var i = 0; i < 1000; i++) {
-  JSON.parse(json);
-}
-console.log("JSON.parse took %s", Date.now() - start);
 
-start = Date.now();
-var p = new Parser();
-for (var i = 0; i < 1000; i++) {
-  p.write(json);
+while (true) {
+  var start = Date.now();
+  for (var i = 0; i < 300; i++) {
+    JSON.parse(json);
+  }
+  var first = Date.now() - start;
+  console.log("JSON.parse took %s", first);
+
+  start = Date.now();
+  var p = new Parser();
+  for (var i = 0; i < 300; i++) {
+    p.write(json);
+  }
+  var second = Date.now() - start;
+  console.log("streaming parser took %s", second);
+  console.log("streaming is %s times slower", second / first);
 }
-console.log("streaming parser took %s", Date.now() - start);
