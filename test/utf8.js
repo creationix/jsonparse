459f513d0278fccc4ead28314c243f5b54190fcd
James Halliday <mail@substack.net>
2012-12-18 22:24:21 -0800

    more passing utf8 tests

--- test/utf8.js
+++ test/utf8.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 var Parser = require('../');
 
-test('utf8', function (t) {
+test('3 bytes of utf8', function (t) {
   t.plan(1);
 
   var p = new Parser();
@@ -10,4 +10,29 @@ test('utf8', function (t) {
   };
 
   p.write('"├──"');
+});
+
+test('utf8 snowman', function (t) {
+  t.plan(1);
+
+  var p = new Parser();
+  p.onValue = function (value) {
+    t.equal(value, '☃');
+  };
+
+  p.write('"☃"');
+});
+
+test('utf8 with regular ascii', function (t) {
+  t.plan(4);
+
+  var p = new Parser();
+  var expected = [ "snow: ☃!", "xyz", "¡que!" ];
+  expected.push(expected.slice());
+
+  p.onValue = function (value) {
+    t.deepEqual(value, expected.shift());
+  };
+
+  p.write('["snow: ☃!","xyz","¡que!"]');
 });
